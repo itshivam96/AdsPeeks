f4f09c47e1bec9f933d7efb7eb03eeac475c8d0c
Shivam Kumar <126212511+itshivam96@users.noreply.github.com>
2024-09-21 16:43:40 +0530

    Update script.js

--- AdsPeek/assets/js/script.js
+++ AdsPeek/assets/js/script.js
@@ -1,4 +1,12 @@
-
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
 /**
  * navbar toggle
  */
